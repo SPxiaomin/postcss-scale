e04fbe3a8827d2c98a5045d701e4474bbea39ea5
gujunmin <gujunmin@bytedance.com>
2020-03-27 19:02:23 +0800

    feat: version 1

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 let postcss = require('postcss')
 
-module.exports = postcss.plugin('postcss-scale', (opts = { }) => {
+let pxRegExp = /\b(\d+(\.\d+)?)px\b/;
+let pxGlobalRegExp = new RegExp(pxRegExp.source, 'g');
+let checkScaleRegExp = /\/\* postcss-scale-px (.*?) \*\//;
+
+module.exports = postcss.plugin('postcss-scale', (opts = { scale: 2 }) => {
 
   // Work with options here
 
   return (root, result) => {
-
     // Transform CSS AST here
-    root.walkRules(function(rule) {
-      console.log('rule', rule)
-      // rule.walkDecls(function(decl) {
-      // });
+
+    if (!checkScaleRegExp.exec(root.source.input.css)) {
+      result.root = root;
+      return;
+    }
+
+    let disableAttributes = [];
+    const disableAttributeString = checkScaleRegExp.exec(root.source.input.css)[1]
+    if (disableAttributeString) {
+      disableAttributes = disableAttributeString.split(',');
+    }
+
+    console.log('disableAttributes', disableAttributes)
+
+    root.walkDecls(function (decl) {
+      if (disableAttributes.indexOf(decl.prop) == -1) {
+        if (/px/.test(decl.value)) {
+          decl.value = decl.value.replace(pxGlobalRegExp, function (match, p1) {
+            return p1 * opts.scale + 'px'
+          })
+        }
+      }
     });
-  }
-})
+
+    result.root = root;
+  };
+});
